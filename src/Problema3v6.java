f95d14ba62acb39e7c158627d06d671d85e35d87
Roberto Luís Rezende <robertodermato@gmail.com>
2020-10-08 17:04:05 -0300

    v viking ou 6 do problema 3 melhorado

--- src/Problema3v6.java
+++ src/Problema3v6.java
@@ -1,27 +1,29 @@
 public class Problema3v6 {
     public static void main(String[] args) {
 
-        int r2=0;
+        int index_palavra=0;
+        int index_frase=0;
         int contador=0;
-        int r4;
+        int teste_de_igualdade;
 
         String palavra = "banana";
         String frase = "banana é bom. coma banana";
 
-    for (int i=0; i<frase.length(); i++) {
-        r4 = frase.charAt(i) - palavra.charAt(r2);
+        for (int i=0; i<frase.length(); i++) {
+            teste_de_igualdade = frase.charAt(index_frase) - palavra.charAt(index_palavra);
 
-        //letra é igual
-        if (r4 == 0) {
-            r2 = r2 + 1;
+            //letra é igual
+            if (teste_de_igualdade == 0) {
+                index_palavra++;
 
-            //chegou no fim da palavra, ou seja achou uma palavra
-            if (r2 == palavra.length()) {
-                contador = contador + 1;
-                r2=0;
+                //chegou no fim da palavra, ou seja achou uma palavra
+                if (index_palavra == palavra.length()) {
+                    contador = contador + 1;
+                    index_palavra=0;
+                }
             }
+            index_frase++;
         }
-    }
         System.out.println(contador);
     }
 }
