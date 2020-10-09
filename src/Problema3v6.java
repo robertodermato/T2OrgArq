9fa6bae5e9386d7bb2dd9b4cebc964817e832d5a
Roberto Luís Rezende <robertodermato@gmail.com>
2020-10-09 20:57:40 -0300

    v viking ou 6 do problema 3 melhorado - agora funciona 100%

--- src/Problema3v6.java
+++ src/Problema3v6.java
@@ -7,26 +7,31 @@ public class Problema3v6 {
         int teste_de_igualdade;
 
         String palavra = "banana";
-        String frase = "banana é bom. Coma bananas";
+        String frase = "banana é bom. Coma sbanana sbanana";
 
-        for (int i = 0; i < frase.length(); i++) {
-            teste_de_igualdade = frase.charAt(index_frase) - palavra.charAt(index_palavra);
-            //letra é igual
-            if (teste_de_igualdade == 0) {
-                index_palavra++;
-                //chegou no fim da palavra, ou seja achou uma palavra
-                if (index_palavra == palavra.length()) {
-                    index_palavra = 0;
-                    //ASCII A=65 Z=90 a=97 z=122
-                    if (index_frase + 1 == frase.length() || frase.charAt(index_frase + 1) < 65 ||
-                            frase.charAt(index_frase + 1) > 122 || (frase.charAt(index_frase + 1) > 90
-                            && frase.charAt(index_frase + 1) < 97)) {
-                        System.out.println(frase.charAt(index_frase + 1));
-                        contador++;
+        for (index_frase = 0; index_frase < frase.length(); index_frase++) {
+
+            if (index_frase==0 || (frase.charAt(index_frase+1)==' ')) {
+
+
+
+                teste_de_igualdade = frase.charAt(index_frase) - palavra.charAt(index_palavra);
+                //letra é igual
+                if (teste_de_igualdade == 0) {
+                    index_palavra++;
+                    //chegou no fim da palavra, ou seja achou uma palavra
+                    if (index_palavra == palavra.length()) {
+
+                        //ASCII A=65 Z=90 a=97 z=122
+                        if (index_frase + 1 == frase.length() || frase.charAt(index_frase + 1) < 65 ||
+                                frase.charAt(index_frase + 1) > 122 || (frase.charAt(index_frase + 1) > 90
+                                && frase.charAt(index_frase + 1) < 97)) {
+                            contador++;
+                            index_palavra = 0;
+                        }
                     }
                 }
             }
-            index_frase++;
         }
         System.out.println(contador);
     }
---
+++ src/Problema3v8.java
@@ -0,0 +1,29 @@
+public class Problema3v8 {
+    public static void main(String[] args) {
+
+        int index_palavra=0;
+        int index_frase=0;
+        int contador=0;
+        int teste_de_igualdade;
+
+        String palavra = "banana";
+        String frase = "banana é bom. coma banana";
+
+        for (int i=0; i<frase.length(); i++) {
+            teste_de_igualdade = frase.charAt(index_frase) - palavra.charAt(index_palavra);
+
+            //letra é igual
+            if (teste_de_igualdade == 0) {
+                index_palavra++;
+
+                //chegou no fim da palavra, ou seja achou uma palavra
+                if (index_palavra == palavra.length()) {
+                    contador = contador + 1;
+                    index_palavra=0;
+                }
+            }
+            index_frase++;
+        }
+        System.out.println(contador);
+    }
+}
